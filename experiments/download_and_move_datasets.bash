e0d60e8f9b4f4e5508ceab7112e9a0b757645df6
Milad-Khormaee <pransermi@gmail.com>
2025-08-21 23:56:43 +0330

    feat(experiments): add download and push datasets to modal volume bash

---
+++ experiments/download_and_move_datasets.bash
@@ -0,0 +1,19 @@
+modal volume create dataset-volume
+
+# python download_datasets.py
+DEFAULT_PATH="/home/itz-amethyst/dev/axcer/experiments/datasets"
+
+# If provided, use the first argument as the upload path; otherwise, fall back to the default.
+if [ -n "$1" ]; then
+  LOCAL_DATA_PATH="$1"
+else
+  LOCAL_DATA_PATH="$DEFAULT_PATH"
+fi
+
+if [ ! -e "$LOCAL_DATA_PATH" ]; then
+  echo "Error: '$LOCAL_DATA_PATH' does not exist."
+  exit 1
+fi
+
+# Perform upload preserving filenames and structure, with overwrite enabled
+modal volume put dataset-volume "$LOCAL_DATA_PATH/" / --force
